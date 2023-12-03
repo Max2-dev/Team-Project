71597e6961f3fe203cb45a06d8c37fea36a697cd
Jhan Valenzuela <210082920@aston.ac.uk>
2023-12-03 16:11:31 +0000

    Added class and id's

--- user_login.php
+++ user_login.php
@@ -5,24 +5,23 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Log In page</title>
 </head>
-<body>
-
-<section>
-    <div>
-      <form>
-        <header>Welcome</header>
+<body class="u_bcolor">
+<section id="login-section">
+    <div class="container">
+      <form class="frm">
+        <header class="header">Welcome</header>
         <p>Log in</p>
         <p>Please fill in this form to log in</p>
         
         <label for="username">Username</label>
-        <input type="text" placeholder="Enter Username" name="username" required>
+        <input type="text" placeholder="Enter Username" class="un_input" name="username" required>
 
         <label for="psw">Password</label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <input type="password" placeholder="Enter Password" class="un_input" name="psw" required>
             
-        <button type="submit" name="login">Log in</button>
+        <button type="submit" name="login" class="frmbtn">Log in</button>
 
-        <span>Create a new Account? <a href="user_signup.php"> Sign up</a></span>
+        <span>Create a new Account? <a href="user_signup.php" class="link"> Sign up</a></span>
         <span>Click to <a href="#"> Log in as Admin</a></span>
         
       </form>
--- user_signup.php
+++ user_signup.php
@@ -5,29 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up Page</title>
 </head>
-<body>
-<section>
-    <div>
-      <form>
-        <header>Welcome to T32 Games</header>
+<body class="u_bcolor">
+<section id="signup-section">
+    <div class="container">
+      <form id="signup" class="frm">
+        <header class="header">Welcome to T32 Games</header>
         <p>Sign Up</p>
         <p>Please fill in this form to sign up</p>
 
         <label for="email">Email Address</label>
-        <input type="email" name="email" placeholder="email" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" required>
+        <input type="email" name="email" id="email" class="un_input" placeholder="email" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" required>
 
         <label for="username">Username</label>
-        <input type="text" placeholder="Enter Username" name="username" required>
+        <input type="text" placeholder="Enter Username" class="un_input" name="username" required>
 
         <label for="psw">Password</label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <input type="password" placeholder="Enter Password" class="un_input" name="psw" required>
 
         <label for="cfmpsw">Confirm Password</label>
-        <input type="password" placeholder="Confirm Password" name="cfmpsw" required>
+        <input type="password" placeholder="Confirm Password" class="un_input" name="cfmpsw" required>
 
-        <button type="submit" name="signedup">Sign Up</button>
+        <button type="submit" class="frmbtn" name="signedup">Sign Up</button>
 
-        <span>Already have an account? <a href="user_login.php"> Log In</a></span>
+        <span>Already have an account? <a href="user_login.php" class="link"> Log In</a></span>
       </form>
     </div>
 </section>
