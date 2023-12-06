9f4932501189ca160f4c1b994d40b3409f357ec7
AdamUlhaq <148955980+AdamUlhaq@users.noreply.github.com>
2023-12-06 10:35:04 +0000

    Add files via upload

---
+++ about.php
@@ -0,0 +1,186 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+   <section id="about-us">
+        <h1>About T32 Games</h1>
+        <p>Welcome to T32 Games, your ultimate destination for cutting-edge gaming technology. We are dedicated to developing and delivering innovative gaming experiences that push the boundaries of creativity and technology.</p>
+        <p>T32 Games has quickly established itself as a leader in the gaming industry. Our team consists of passionate developers, designers, and gaming enthusiasts who are committed to creating immersive and engaging games for players around the world.</p>
+        <p>At T32 Games, we believe in harnessing the power of technology to create games that are not only entertaining but also offer unique and memorable experiences. Our portfolio includes a variety of games across different genres, each designed with our signature touch of innovation and excellence.</p>
+        <p>We are constantly exploring new technologies and trends to ensure that our games remain at the forefront of the gaming industry. Join us on our journey as we continue to redefine gaming and bring our vision to life.</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">client's reviews</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-1.png" alt="">
+         <p>Nice! Good products.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Alice</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-2.png" alt="">
+         <p>Really good products</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Deona</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-3.png" alt="">
+         <p>Super</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>jhon</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-4.png" alt="">
+         <p>super easy to order</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Riya</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.png" alt="">
+         <p>very safe place to order</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>jimmy</h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-6.png" alt="">
+         <p>Esay to make Account</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>leena</h3>
+      </div>
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
