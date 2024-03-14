62d480cb7d3c337b9ebe8ab03775e63299407cb7
Jhan Valenzuela <210082920@aston.ac.uk>
2024-03-14 10:16:33 +0000

    Added purchasing_order sql file

---
+++ purchasing_order_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE purchasing (
+    id int(10) NOT NULL,
+    supplier VARCHAR(256) NOT NULL,
+    destination VARCHAR(256) NOT NULL,
+    product_request VARCHAR(256) NOT NULL,
+    quantity int(10) NOT NULL,
+    status VARCHAR(256) NOT NULL
+    );
+
+
+
+
