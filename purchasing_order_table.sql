1ffc36defa5e378b75f4baecc4622232384efe0c
Jhan Valenzuela <148958644+TPTM32-Jhan@users.noreply.github.com>
2024-03-22 14:30:07 +0000

    Update purchasing_order_table.sql
    Products table for the database derived from Datt patel

--- purchasing_order_table.sql
+++ purchasing_order_table.sql
@@ -8,5 +8,14 @@ CREATE TABLE purchasing (
     );
 
 
-
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `details` varchar(500) NOT NULL,
+  `price` int(10) NOT NULL,
+  `image_01` varchar(100) NOT NULL,
+  `image_02` varchar(100) NOT NULL,
+  `image_03` varchar(100) NOT NULL,
+  `Category_id` int(11) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
